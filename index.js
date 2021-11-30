6369b44bfe87220e79c7cf941f188c6c270ca56c
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-11-30 20:26:08 -0300

    fix: change the start comand

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server =jsonServer.create();
 
 const routes =jsonServer.router('db.json');
 
-console.log("test")
+console.log(test)
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
